d8feb05e0440e1617dde05300ce9f159a77fbcd2
Jaedon Braun <jaedonbraun@gmail.com>
2020-03-06 14:33:20 -0800

    Changes to be overwritten

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 /* Scripts Go Here */
+
+let word = "apple";
+let showWord = ["*","*","*","*","*"];
+let guesses = [];
+
+
+function getLetter(guess) {
+    let numberOfLetters = 0;
+
+    for (let i=0; i < word.length; i++) {
+        if (word.charAt(i) == guess) {
+            numberOfLetters++;
+            showWord[i] = word.charAt(i);
+        }
+    }
+
+}
+
+function updateWord() {
+
+    let value = "";
+    for (let i=0; i < showWord.length; i++) {
+        value += showWord[i];
+    }
+    document.getElementById("word").innerHTML = value;
+}
