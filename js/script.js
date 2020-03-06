8036c19a5da746f88578ca700cf3eb463d161300
Samuel Shannon <sshannon237@gmail.com>
2020-03-06 12:27:49 -0800

    creating button code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-/* Scripts Go Here */
+let word= "TEST";
+function buildButton() {
+    for (let i = 65; i < 91; i++) {
+        let newButton = document.createElement("input");
+        let ch = String.fromCharCode(i)
+        newButton.type = "button";
+        newButton.value = ch;
+        newButton.onclick = buttonPress(ch,newButton);
+        document.body.appendChild(newButton);
+    }
+}
+
+function buttonPress(letter,button) {
+    return function () {
+        button.disabled="disabled";
+        checkLetter(letter);
+    }
+}
+
+function checkLetter(letter){
+    if (word.indexOf(letter) == -1){
+        //badGuess();
+        console.log(letter);
+    } else{
+        //goodGuess();
+        for(let i = 0; i < word.length; i++){
+            if (letter == word.charAt(i)){
+                console.log(i);
+            }
+        }
+    }
+}
+
+function goodGuess(){
+    
+}
+buildButton();
